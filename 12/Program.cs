0ddc21319b81f96d801a37e25817e418af23544d
rwbxd <will.bryan99@gmail.com>
2023-12-15 23:59:48 -0600

    Day 12 Part 1.95

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text;
 
 string input = "C:\\Users\\Will\\Documents\\code\\advent2023\\12\\input";
 StreamReader reader = new StreamReader(input);
@@ -15,9 +13,9 @@ while ((line = reader.ReadLine()!) != null) {
     Console.WriteLine(iter++.ToString() + " | " + TimeSpan.FromSeconds((DateTime.Now - startTime).TotalSeconds).ToString(@"hh\:mm\:ss\:fff"));
     string[] segments = line.Split();
     string record = segments[0].Trim('.');
-    record = string.Join('?', Enumerable.Repeat(segments[0], 5));
+    // record = string.Join('?', Enumerable.Repeat(segments[0], 5));
     string[] nums = segments[1].Split(',');
-    nums = Enumerable.Repeat(nums, 5).SelectMany(x => x).ToArray();
+    // nums = Enumerable.Repeat(nums, 5).SelectMany(x => x).ToArray();
     long poss = findPossibilities(record, nums);
     Console.WriteLine("Possibilities: " + poss);
     sum += poss;
@@ -59,61 +57,89 @@ string depthTabs(int depth) {
     return string.Join("", Enumerable.Repeat("\t", depth));
 }
 
-long findPossibilities(string x, string[] nums, int depth=0, string label="") {
-    Console.WriteLine(x);
-    // Console.WriteLine(depthTabs(depth) + x + " | [" + string.Join(",", nums) + "]");
-    x = x.Trim('.');
-    // Console.WriteLine(string.Join(" | ", [x, nums2key(nums), depth.ToString(), label]));
-    if (cache.ContainsKey(x) && cache[x].ContainsKey(nums2key(nums))) {
-        // Console.WriteLine(depthTabs(depth) + "Returning cached result: " + cache[x][nums2key(nums)].ToString());
-        return cache[x][nums2key(nums)];
+bool isValidString(string x, string[] nums) {
+    // Console.WriteLine(string.Join(',', x.Split('.', StringSplitOptions.RemoveEmptyEntries)));
+    if (x.Split(".", StringSplitOptions.RemoveEmptyEntries).Length != nums.Length) { return false; }
+    int i = 0;
+    // Console.WriteLine("BBB");
+    foreach (string s in x.Split('.', StringSplitOptions.RemoveEmptyEntries)) {
+        // Console.WriteLine(i.ToString() + " " + s + " " + nums[i]);
+        if (s.Length != int.Parse(nums[i])) { return false; }
+        i++;
     }
+    return true;
+}
 
-    // generateRegexString is our "is this string possible" string - if impossible, return 0
-    // if (!Regex.Match(x, generateRegexString(nums)).Success) {
-    //     // Console.WriteLine(depthTabs(depth + 1) + "Invalid string.");
-    //     return 0;
-    // }
+long findPossibilities(string x, string[] nums, int depth=0, string label="") {
+    // Console.WriteLine(string.Join("", Enumerable.Repeat('\t', depth)) + x + " -> " + nums2key(nums));
+    x = x.Trim('.');
+    if (cache.ContainsKey(x) && cache[x].ContainsKey(nums2key(nums))) return cache[x][nums2key(nums)];
+
+    if (x == "") {
+        if (nums.Length == 0) {
+            return 1;
+        }
+        return 0;
+    }
 
     // if there's no #'s needed, return the 1 possibility (all '.'s)
-    // also, if there's no possible variations (and we know it's valid per above), return the 1 possibility
-    if (nums.Length == 0 || !x.Contains('?')) {
-        // Console.WriteLine(depthTabs(depth + 1) + "Valid string.");
-        // return 1;
-
-        if (!Regex.Match(x, generateRegexString(nums)).Success) {
-        // Console.WriteLine(depthTabs(depth + 1) + "Invalid string.");
-            return 0;
-        }
-        return 1;
+    if (nums.Length == 0) {
+        if (x.Contains('#')) { return cacheAndReturn(x, nums, 0); }
+        else { return cacheAndReturn(x, nums, 1); }
     }
 
+    // If length < sum(nums), there's no possible combination
+    if (x.Length < nums.Select(int.Parse).Sum() + nums.Length - 1) { return cacheAndReturn(x, nums, 0); }
+
+    // If there's no '?', then the string should match
+    if (!x.Contains('?')) {
+        // Console.WriteLine("AHH");
+        isValidString(x, nums);
+        if (isValidString(x, nums)) { return cacheAndReturn(x, nums, 1); }
+        else { return cacheAndReturn(x, nums, 0); }
+    }
+
+    int firstNum = int.Parse(nums[0]);
     if (x[0] == '?') {
         long result = 0;
-        result += findPossibilities(x.Remove(0, 1).Insert(0, "."), nums, depth + 1, "?=.") + findPossibilities(x.Remove(0, 1).Insert(0, "#"), nums, depth + 1, "?=#");
-        // Console.WriteLine(depthTabs(depth + 1) + "Adding cache[" + x + "] with (" + nums2key(nums) + ", " + result.ToString() + ")");
-        // Console.WriteLine(depthTabs(depth + 1) + "Found: " + result.ToString());
+        
+        // Case where ? becomes '.'
+        result += findPossibilities(x[1..], nums, depth + 1, "?=.");
+
+        if (x.Length < firstNum) {return cacheAndReturn(x, nums, result);}
+        // Case where ? becomes '#'
+        if (x.Length == firstNum) {
+            result += findPossibilities(x[(firstNum)..], nums[1..], depth + 1, "?=#");
+        } else {
+            if (x[(firstNum)] == '#') {return cacheAndReturn(x, nums, result);}
+            result += findPossibilities(x[(firstNum+1)..], nums[1..], depth + 1, "?=#");
+        }
         return cacheAndReturn(x, nums, result);
     } else { // x[0] == '#'
-        if (!Regex.Match(x, generateRegexString(nums)).Success) {
-        // Console.WriteLine(depthTabs(depth + 1) + "Invalid string.");
-            return 0;
+        if (nums.Length == 1) {
+            for (int i = 1; i < firstNum; i++) {
+                if (x[i] == '.') { return cacheAndReturn(x, nums, 0); } // Invalid for ? to equal #
+            }
+            for (int i = firstNum; i < x.Length; i++) {
+                if (x[i] == '#') { return cacheAndReturn(x, nums, 0); } // Invalid for there to be a # after the intial sequence
+            }
+            return cacheAndReturn(x, nums, 1);
         }
-        if (nums.Length == 1) { return 1; }
-        if (!Regex.Match(x, generateRegexString(nums)).Success) {
-        // Console.WriteLine(depthTabs(depth + 1) + "Invalid string.");
-            return 0;
+
+        if (x.Length < firstNum+1) {return cacheAndReturn(x, nums, 0);}
+        for (int i = 1; i < firstNum; i++) {
+            if (x[i] == '.') { return cacheAndReturn(x, nums, 0); } // Invalid for ? to equal #
         }
-        long result = findPossibilities(x[(int.Parse(nums[0]) + 1)..], nums[1..], depth + 1, "del #");
-        // Console.WriteLine(depthTabs(depth + 1) + "Adding cache[" + x + "] with (" + nums2key(nums) + ", " + result.ToString() + ")");
-        // Console.WriteLine(depthTabs(depth + 1) + "Found: " + result.ToString());
-        return cacheAndReturn(x, nums, result);
+
+        if (x[(firstNum)] == '#') {return cacheAndReturn(x, nums, 0);}
+        return cacheAndReturn(x, nums, findPossibilities(x[(firstNum+1)..], nums[1..], depth + 1, "del #"));
     }
 }
 
 long cacheAndReturn(string x, string[] nums, long v) {
     if (!cache.ContainsKey(x)) {cache.Add(x, new Dictionary<string, long>());}
     if (!cache[x].ContainsKey(nums2key(nums))) {cache[x].Add(nums2key(nums), v);}
+    // if (v != 0) Console.WriteLine("Caching " + x + " " + string.Join(",", nums) + ": " + v.ToString());
     return v;
 }
 
