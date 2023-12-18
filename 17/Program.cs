4522f1b2f463d6e35fa3b2985db9d472170eb4c9
rwbxd <will.bryan99@gmail.com>
2023-12-18 11:12:16 -0600

    Day 17 Part 2

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Globalization;
-using System.Security.Cryptography;
-
-string input = "C:\\Users\\Will\\Documents\\code\\advent2023\\17\\input";
+﻿string input = "C:\\Users\\Will\\Documents\\code\\advent2023\\17\\input";
 StreamReader reader = new StreamReader(input);
 
 List<List<int>> grid = new List<List<int>>();
@@ -116,7 +113,104 @@ long part1(List<List<int>> grid) {
 }
 
 long part2(List<List<int>> grid) {
-    return 0;
+    List<List<Node>> nodeGrid = new List<List<Node>>(grid.Count);
+    for (int i = 0; i < grid.Count; i++) {
+        List<Node> nodeRow = new List<Node>(grid[0].Count);
+        for (int j = 0; j < grid[0].Count; j++) {
+            nodeRow.Add(new Node(i, j));
+        }
+        nodeGrid.Add(nodeRow);
+    }
+    Node goal = nodeGrid[grid.Count - 1][grid[0].Count - 1];
+    
+    // Dictionary<NodeNode, int> cost = new(); // Cost to get there, not including self
+    PriorityQueue<NodeNode, int> pq = new();
+    NodeNode nn;
+    NodeNode zero = new NodeNode(nodeGrid[0][0], '?', 0, 0);
+    nn = createNodeNode4(zero, nodeGrid[0][1], 'R');
+    pq.Enqueue(nn, nn.Cost + dist(nn.N, goal));
+    // cost[nn] = 0;
+    nn = createNodeNode4(zero, nodeGrid[1][0], 'D');
+    pq.Enqueue(nn, nn.Cost + dist(nn.N, goal));
+    // cost[nn] = 0;
+
+    bool foo(int i, int j, int nextCost) {
+        return bq.check(i, j);
+        // return bq.check(i, j) && (!cost.ContainsKey(nodeGrid[i][j]) || cost[nodeGrid[i][j]] > nextCost);
+    }
+
+    bool bar(NodeNode n, char d) {
+        if (n.D == d) {
+            return n.CD is >= 4 and < 10;
+        }
+
+        switch (n.D) {
+            case 'U': return d != 'D';
+            case 'R': return d != 'L';
+            case 'L': return d != 'R';
+            case 'D': return d != 'U';
+        }
+        
+        return false;
+    }
+
+    HashSet<string> seen = new HashSet<string>();
+    NodeNode curNN = new NodeNode(nodeGrid[0][0], '?', 0, 0);
+    while (pq.Count != 0) {
+        
+        curNN = pq.Dequeue();
+        Node cur = curNN.N;
+
+        if (seen.Contains(curNN.ToString())) {continue;}
+        seen.Add(curNN.ToString());
+
+        Console.WriteLine(string.Format("({1}, {2}): {0} | {3}", curNN.Cost, cur.I, cur.J, curNN.D));
+        
+        if (cur.I == goal.I && cur.J == goal.J) {
+            Console.WriteLine("Found goal");
+            break;
+        }
+
+        // int nextCost = cost[cur] + grid[cur.I][cur.J];
+        int nextCost = curNN.Cost + grid[cur.I][cur.J];
+        int ci = cur.I; int cj = cur.J;
+        NodeNode newNN;
+
+        if (bar(curNN, 'D') && foo(ci+1, cj, nextCost) && !(curNN.D != 'D' && !foo(ci+4, cj, nextCost))) {
+            newNN = createNodeNode4(curNN, nodeGrid[ci+1][cj], 'D');
+            Console.WriteLine(string.Format("Adding ({0}, {1}) 'D' with cost: {2}", newNN.N.I, newNN.N.J, newNN.Cost));
+            pq.Enqueue(newNN, newNN.Cost + dist(newNN.N, goal));
+        }
+
+        if (bar(curNN, 'U') && foo(ci-1, cj, nextCost) && !(curNN.D != 'U' && !foo(ci-4, cj, nextCost))) {
+            // cost[nodeGrid[ci-1][cj]] = nextCost;
+            newNN = createNodeNode4(curNN, nodeGrid[ci-1][cj], 'U');
+            Console.WriteLine(string.Format("Adding ({0}, {1}) 'U' with cost: {2}", newNN.N.I, newNN.N.J, newNN.Cost));
+            pq.Enqueue(newNN, newNN.Cost + dist(newNN.N, goal));
+        }
+
+        if (bar(curNN, 'R') && foo(ci, cj+1, nextCost) && !(curNN.D != 'R' && !foo(ci, cj+4, nextCost))) {
+            // cost[nodeGrid[ci][cj+1]] = nextCost;
+            newNN = createNodeNode4(curNN, nodeGrid[ci][cj+1], 'R');
+            Console.WriteLine(string.Format("Adding ({0}, {1}) 'R' with cost: {2}", newNN.N.I, newNN.N.J, newNN.Cost));
+            pq.Enqueue(newNN, newNN.Cost + dist(newNN.N, goal));
+        }
+
+        if (bar(curNN, 'L') && foo(ci, cj-1, nextCost) && !(curNN.D != 'L' && !foo(ci, cj-4, nextCost))) {
+            // cost[nodeGrid[ci][cj-1]] = nextCost;
+            newNN = createNodeNode4(curNN, nodeGrid[ci][cj-1], 'L');
+            Console.WriteLine(string.Format("Adding ({0}, {1}) 'L' with cost: {2}", newNN.N.I, newNN.N.J, newNN.Cost));
+            pq.Enqueue(newNN, newNN.Cost + dist(newNN.N, goal));
+        }
+    }
+
+    // foreach (var item in cost) {
+    //     Console.WriteLine(string.Format("({0}, {1}): {2}", item.Key.I, item.Key.J, item.Value+grid[item.Key.I][item.Key.J]));
+    // }
+
+    Console.WriteLine("\n" + curNN.ToString());
+
+    return curNN.Cost;// + grid[curNN.N.I][curNN.N.J];
 }
 
 int dist(Node a, Node b) {
@@ -130,6 +224,28 @@ NodeNode createNodeNode(NodeNode cur, Node next, char dir) {
         return new NodeNode(next, dir, 1, cur.Cost + grid[next.I][next.J]);
     } else {
         return new NodeNode(next, dir, cur.CD + 1, cur.Cost + grid[next.I][next.J]);
+    }
+}
+
+NodeNode createNodeNode4(NodeNode cur, Node next, char dir) {
+    if (cur.D == dir) {
+        return new NodeNode(next, dir, cur.CD + 1, cur.Cost + grid[next.I][next.J]);
+    }
+
+    if (cur.N.I == next.I) {
+        int newCost = grid[next.I][next.J];
+        for (int j = 1; j < 4; j++) {
+            newCost += grid[next.I][next.J + ((next.J - cur.N.J) * j)];
+        }
+        Console.WriteLine("new cost: " + newCost.ToString());
+        return new NodeNode(new Node(next.I, next.J + ((next.J - cur.N.J) * 3)), dir, 4, cur.Cost + newCost);
+    } else {
+        int newCost = grid[next.I][next.J];
+        for (int i = 1; i < 4; i++) {
+            newCost += grid[next.I + ((next.I - cur.N.I) * i)][next.J];
+        }
+        Console.WriteLine("new cost: " + newCost.ToString());
+        return new NodeNode(new Node(next.I + ((next.I - cur.N.I) * 3), next.J), dir, 4, cur.Cost + newCost);
     }
 }
 
