b00dc9c27b6130d828ca0a0dcdc47f6c52122af4
rwbxd <will.bryan99@gmail.com>
2023-12-19 13:28:08 -0600

    Day 18

--- 18/Program.cs
+++ 18/Program.cs
@@ -143,30 +143,75 @@ void Part2() {
 
     Console.WriteLine(string.Join(",", lens));
 
+    long area = 0;
     int curWidth = 1;
     int curLength = 1;
     int R = 0; int L = 0; int U = 0; int D = 0;
-    HashSet<string> hashmarks = new HashSet<string>();
-    hashmarks.Add("(0,0)");
+    Dictionary<int, HashSet<int>> hashmarks = new Dictionary<int, HashSet<int>>{
+        { 0, new HashSet<int>(0) }
+    };
+    Dictionary<int, HashSet<int>> hashcorners = new Dictionary<int, HashSet<int>>{
+        { 0, new HashSet<int>(0) }
+    };
+    
     for (int i = 0; i < dirs.Count; i++) {
+        area += lens[i];
         switch (dirs[i]) {
             case 'R':
-                for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength, curWidth+x+1));}
+                // for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength, curWidth+x+1));}
+                if (!hashmarks.ContainsKey(curLength)) {hashmarks[curLength] = new();}
+                if (!hashcorners.ContainsKey(curLength)) {hashcorners[curLength] = new();}
+                // hashmarks[curLength].Add(curWidth + 1);
+                // hashcorners[curLength].Add(curWidth + 1);
+                hashmarks[curLength].Add(curWidth + lens[i]);
+                // hashcorners[curLength].Add(curWidth + lens[i]);
                 curWidth += lens[i];
                 R = Math.Max(R, curWidth);
                 break;
             case 'L':
-                for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength, curWidth-x-1));}
+                // for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength, curWidth-x-1));}
+                if (!hashmarks.ContainsKey(curLength)) {hashmarks[curLength] = new();}
+                if (!hashcorners.ContainsKey(curLength)) {hashcorners[curLength] = new();}
+                // hashmarks[curLength].Add(curWidth - 1);
+                // hashcorners[curLength].Add(curWidth - 1);
+                hashmarks[curLength].Add(curWidth - lens[i]);
+                // hashcorners[curLength].Add(curWidth - lens[i]);
                 curWidth -= lens[i];
                 L = Math.Max(L, curWidth * -1);
                 break;
             case 'U':
-                for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength-x-1, curWidth));}
+                // for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength-x-1, curWidth));}
+                if (!hashcorners.ContainsKey(curLength)) {hashcorners[curLength] = new();}
+                hashcorners[curLength].Add(curWidth);
+
+                for (int x = 1; x <= lens[i]; x++) {
+                    if (!hashmarks.ContainsKey(curLength - x)) {
+                        hashmarks[curLength - x] = new();
+                    }
+                    hashmarks[curLength - x].Add(curWidth);
+                }
+
+                if (!hashcorners.ContainsKey(curLength - lens[i])) {hashcorners[curLength - lens[i]] = new();}
+                hashcorners[curLength - lens[i]].Add(curWidth);
+
                 curLength -= lens[i];
                 U = Math.Max(U, -1 * curLength);
                 break;
             case 'D':
-                for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength+x+1, curWidth));}
+                // for (int x = 0; x < lens[i]; x++) {hashmarks.Add(string.Format("({0},{1})", curLength+x+1, curWidth));}
+                if (!hashcorners.ContainsKey(curLength)) {hashcorners[curLength] = new();}
+                hashcorners[curLength].Add(curWidth);
+
+                for (int x = 1; x <= lens[i]; x++) {
+                    if (!hashmarks.ContainsKey(curLength + x)) {
+                        hashmarks[curLength + x] = new();
+                    }
+                    hashmarks[curLength + x].Add(curWidth);
+                }
+
+                if (!hashcorners.ContainsKey(curLength + lens[i])) {hashcorners[curLength + lens[i]] = new();}
+                hashcorners[curLength + lens[i]].Add(curWidth);
+
                 curLength += lens[i];
                 D = Math.Max(D, curLength);
                 break;
@@ -178,28 +223,50 @@ void Part2() {
     width = R + L + 1;
     int curI = U+1; int curJ = L+1;
 
-    long area = 0;
-    bool inside;
-    for (int i = 0; i < length; i++) {
-        Console.WriteLine("Row " + i.ToString() + "/" + length.ToString());
-        inside = false;
-        for (int j = 0; j < width; j++) {
-            if (hashmarks.Contains(string.Format("({0},{1})", i, j))) {
-                area++;
-                if (!hashmarks.Contains(string.Format("({0},{1})", i, j-1)) && !hashmarks.Contains(string.Format("({0},{1})", i, j+1))) {
-                    inside = !inside;
-                } else {
-                    if (!hashmarks.Contains(string.Format("({0},{1})", i-1, j))) {
-                        inside = !inside;
-                    }
-                }
-            } else {
-                if (inside) {
-                    area++;
-                }
-            }
+    foreach (var i in hashcorners.Keys) {
+        foreach (var j in hashcorners[i]) {
+            Console.WriteLine(string.Format("{0}, {1}", i, j));
         }
     }
 
+    bool inside;
+    List<int> ikeys = hashmarks.Keys.ToList();
+    ikeys.Sort();
     Console.WriteLine(area);
+    foreach (int i in ikeys) {
+        if (i % 1_000_000 == 0) Console.WriteLine("Row " + i.ToString() + "/" + length.ToString());
+        inside = false;
+        if (!hashmarks.ContainsKey(i)) continue;
+
+        List<int> hsl = hashmarks[i].ToList();
+        hsl.Sort();
+        int prev = 0;
+        bool ignore = false;
+        foreach (var j in hsl) { // j = j value of hashmark
+            // Console.WriteLine(string.Format("{0} - in: {1}, ig: {2}, prev: {3}", j, inside, ignore, prev));
+            if (inside && !ignore) {
+                area += j - prev - 1;
+            }
+            if (isCorner(i,j)) {
+                ignore = !ignore;
+
+                if (hasNorth(i,j)) {inside = !inside;}
+            } else {
+                inside = !inside;
+            }
+            // Console.WriteLine(string.Format("{4} | {0},{1} - {2} ({3})", i, j, ignore, j - prev - 1, area));
+            prev = j;
+        }
+        // Console.WriteLine(area);
+    }
+
+    Console.WriteLine(area);
+
+    bool isCorner(int i, int j) {
+        return hashcorners.ContainsKey(i) && hashcorners[i].Contains(j);
+    }
+
+    bool hasNorth(int i, int j) {
+        return hashmarks.ContainsKey(i-1) && hashmarks[i-1].Contains(j);
+    }
 }
