79b1f8d67408189cab4580e6a090a16c3d60dd74
rwbxd <will.bryan99@gmail.com>
2023-12-20 11:11:23 -0600

    Add template

---
+++ 00/Template.cs
@@ -0,0 +1,12 @@
+int day = 0
+string input = "C:\\Users\\Will\\Documents\\code\\advent2023\\" + day + "\\input";
+Part1();
+Part2();
+
+void Part1() {
+    StreamReader reader = new StreamReader(input);
+}
+
+void Part2() {
+    StreamReader reader = new StreamReader(input);
+}
