f7b7f994036c61e356519a6031315ad26386466d
rwbxd <will.bryan99@gmail.com>
2023-12-24 01:24:29 -0600

    Day 24 Part 1.33

---
+++ 24/Program.cs
@@ -0,0 +1,186 @@
+﻿int day = 24;
+string input = "C:\\Users\\Will\\Documents\\code\\advent2023\\" + day + "\\input";
+Part1();
+Part2();
+
+void Part1() {
+    StreamReader reader = new StreamReader(input);
+    List<Hail> h = new List<Hail>();
+    string line;
+    while ((line = reader.ReadLine()!) != null) {
+        h.Add(new Hail(line));
+    }
+
+    foreach (Hail piece in h) {
+        Console.WriteLine(piece);
+    }
+
+    int count = 0;
+    long start = 200000000000000;
+    long end = 400000000000000;
+    // long start = 7;
+    // long end = 27;
+    for (int h1i = 0; h1i < h.Count; h1i++) {
+        var h1 = h[h1i];
+        for (int h2i = h1i+1; h2i < h.Count; h2i++) {
+            var h2 = h[h2i];
+
+            if (h1.slope == h2.slope) {continue;}
+
+            double intersectX = (h2.b - h1.b) / (h1.slope - h2.slope);
+            double intersectY = h1.FindY(intersectX);
+            // double h1time = (intersectY - h1.py) / h1.vy;
+            // Console.WriteLine(string.Format("{0} + {1} * {2} = {3}", h1.py, (intersectY - h1.py), h1.vy, intersectY));
+            // Console.WriteLine(string.Format("{0} + {1} * {2} = {3}", h2.py, (intersectY - h2.py), h2.vy, intersectY));
+            // double h2time = (intersectY - h2.py) / h2.vy;
+            if (start <= intersectX && intersectX <= end) {
+                if (start <= intersectY && intersectY <= end) {
+                    if (!pastPoint(intersectX, h1.px, h1.vx) && !pastPoint(intersectX, h2.px, h2.vx)) {
+                        Console.WriteLine(string.Format("{0} and {1} cross at ({2}, {3})", h1, h2, intersectX, intersectY));
+                        count++;
+                    }
+                    // Console.WriteLine(h1time + " | " + h2time);
+                    // if (h1time == h2time) {
+                    //     Console.WriteLine(string.Format("{0} and {1} cross at ({2}, {3})", h1, h2, intersectX, intersectY));
+                    //     count++;
+                    // }       
+                }
+            }
+        }
+    }
+    Console.WriteLine(count);
+}
+
+void Part2() {
+    StreamReader reader = new StreamReader(input);
+    List<Hail> h = new List<Hail>();
+    string line;
+    while ((line = reader.ReadLine()!) != null) {
+        h.Add(new Hail(line));
+    }
+
+    int secondTimestep = 2;
+    while (true) {
+        Console.WriteLine("secondTimestep = " + secondTimestep);
+        for (int firstTimestep = 1; firstTimestep < secondTimestep; firstTimestep++) {
+            Console.WriteLine("firstTimestep = " + firstTimestep);
+            for (int h1i = 0; h1i < h.Count; h1i++) {
+                var h1 = h[h1i].PositionAtTime(firstTimestep);
+                // Console.WriteLine("Testing " + h[h1i] + " with collision time " + firstTimestep);
+                for (int h2i = 0; h2i < h.Count; h2i++) {
+                    if (h1i == h2i) {continue;}
+                    // Console.WriteLine("\tTesting " + h[h2i] + " with collision time " + secondTimestep);
+                    var h2 = h[h2i].PositionAtTime(secondTimestep);
+                    HashSet<int> seenTimesteps = [h1i, h2i];
+                    Hail expecting = new Hail(h2.px, h2.py, h2.pz, (h2.px - h1.px) / (secondTimestep - firstTimestep), (h2.py - h1.py) / (secondTimestep - firstTimestep), (h2.pz - h1.pz) / (secondTimestep - firstTimestep));
+                    expecting = expecting.Next();
+
+                    bool valid = true;
+                    int thirdTimestep = secondTimestep+1;
+                    for (int h3i = 0; h3i < h.Count; h3i++) {
+                        if (h3i == h1i || h3i == h2i) {continue;}
+                        var h3 = h[h3i].PositionAtTime(thirdTimestep);
+                        var eCopy = expecting.Copy();
+
+                        int compareX = eCopy.px.CompareTo(h3.px);
+                        int compareY = eCopy.py.CompareTo(h3.px);
+                        int compareZ = eCopy.pz.CompareTo(h3.pz);
+                        while(compareX == eCopy.px.CompareTo(h3.px) && compareY == eCopy.py.CompareTo(h3.px) && compareZ == eCopy.pz.CompareTo(h3.pz)) {
+                            if (h3.Equals(eCopy)) {
+                                if (seenTimesteps.Contains(thirdTimestep)) {
+                                    valid = false;
+                                    break;
+                                } else {
+                                    seenTimesteps.Add(thirdTimestep);
+                                    thirdTimestep = secondTimestep+1;
+                                    break;
+                                }
+                            }
+                            eCopy = eCopy.Next();
+                            h3 = h3.Next();
+                            thirdTimestep++;
+                        }
+                        if (h3.Equals(eCopy)) {
+                            if (seenTimesteps.Contains(thirdTimestep)) {
+                                valid = false;
+                                break;
+                            } else {
+                                seenTimesteps.Add(thirdTimestep);
+                                thirdTimestep = secondTimestep+1;
+                                break;
+                            }
+                        }
+                        if (!valid) {break;}
+                    }
+                    if (valid) {
+                        Console.WriteLine(h1.px - (expecting.vx * firstTimestep) + h1.py - (expecting.vy * firstTimestep) + h1.pz - (expecting.vz * firstTimestep));
+                        return;
+                    }
+                }
+            }
+        }
+        secondTimestep++;
+    }
+
+}
+
+bool pastPoint(double intersectX, double px, double slope) {
+    if (slope > 0) {
+        return px > intersectX;
+    } else {
+        return px < intersectX;
+    }
+}
+
+bool IsInteger(double d) {
+    return Math.Abs(d % 1) <= (double.Epsilon * 100);
+}
+
+class Hail{
+    public double px; public double py; public double pz;
+    public double vx; public double vy; public double vz;
+    public double slope;
+    public double b;
+    public Hail(string s) {
+        double[] pos = s.Split('@')[0].Trim().Split(',').Select(double.Parse).ToArray();
+        double[] vel = s.Split('@')[1].Trim().Split(',').Select(double.Parse).ToArray();
+        px = pos[0]; py = pos[1]; pz = pos[2];
+        vx = vel[0]; vy = vel[1]; vz = vel[2];
+        slope = vy / vx;
+        b = py - (slope * px);
+    }
+    
+    public Hail(double px, double py, double pz) {
+        this.px = px; this.py = py; this.pz = pz;
+    }
+    
+    public Hail(double px, double py, double pz, double vx, double vy, double vz) {
+        this.px = px; this.py = py; this.pz = pz;
+        this.vx = vx; this.vy = vy; this.vz = vz;
+    }
+
+    public Hail Copy() {
+        return PositionAtTime(0);
+    }
+
+    public double FindY(double x) {
+        return slope * x + b;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0}, {1}, {2} @ {3}, {4}, {5}", px, py, pz, vx, vy, vz);
+    }
+
+    public Hail PositionAtTime(int time) {
+        return new Hail(px + vx * time, py + vy * time, pz + vz * time, vx, vy, vz);
+    }
+
+    public Hail Next() {
+        return PositionAtTime(1);
+    }
+    
+    public bool Equals(Hail o) {
+        return px == o.px && py == o.py && pz == o.pz;
+    }
+}
